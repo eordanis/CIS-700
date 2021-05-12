b8dd0ad95db9e0066a7a8657dae7d2644cbfcdee
Stephanie Eordanidis <32005995+eordanis@users.noreply.github.com>
2021-05-12 07:55:25 -0400

    Update visual.py
    updating visual.py to be more self sufficient. will use specified directory else default indicated to read and store from/to. be advised, csv file metrics should be of save row count and labeling

--- utils/visual.py
+++ utils/visual.py
@@ -36,101 +36,73 @@ def display_synth_data(directory=None):
 
 
 def display_metrics(directory=None):
-    df_list = []
-    labels = []
 
+    df_list = []
+    real_df_list = []
+    oracle_df_list = []
+    real_labels = []
+    oracle_labels = []
+    
     if directory is None:
         directory = '/content/CIS-700/results/'
 
     for filename in os.listdir(directory):
         if filename.startswith(experiment_pref) and filename.endswith(csv_ext):
             fn_split = filename.split(experiment_pref)[1].split(csv_ext)[0].split('-')
-            if len(fn_split) == 2:
+            if(len(fn_split) == 2):
                 model = fn_split[0]
                 training = fn_split[1]
-                idx = 4
+                df = pd.read_csv(directory + filename)
                 if training == 'real':
-                    idx = 3
-                #in progress
-                df = pd.read_csv(directory + filename, sep="\n", header=None)  # .iloc[:, :idx]
-                df_list.append(df)
-                labels.append(model)
+                    df = df.rename(columns={"EmbeddingSimilarity": "EmbSim_" + model.capitalize(), "nll-test": "Nll-Test_" + model.capitalize()})
+                    real_df_list.append(df.set_index('epochs'))
+                    real_labels.append(model)
+                elif training == 'oracle':
+                    df = df.rename(columns={"EmbeddingSimilarity": "EmbSim_" + model.capitalize(), "nll-test": "Nll-Test_" + model.capitalize(), "nll-oracle": "Nll-Oracle_" + model.capitalize()})
+                    oracle_df_list.append(df.set_index('epochs'))
+                    oracle_labels.append(model)
+                #TODO Add CFG Training Logic Here
 
-    # prep the metric dataframes
-    oracle_sg = pd.read_csv(directory + 'experiment-log-seqgan-oracle.csv').iloc[:, : 4]
-    oracle_tg = pd.read_csv(directory + 'experiment-log-textgan-oracle.csv').iloc[:, : 4]
-    oracle_cg = pd.read_csv(directory + 'experiment-log-cgan-oracle.csv').iloc[:, : 4]
-    oracle_ig = pd.read_csv (directory + 'experiment-log-infogan-oracle.csv').iloc[:, : 4]
-    oracle_dg = pd.read_csv (directory + 'experiment-log-dcgan-oracle.csv').iloc[:, : 4]
+    real_results = pd.concat(real_df_list)
+    oracle_results = pd.concat(oracle_df_list)
 
-    real_sg = pd.read_csv(directory + 'experiment-log-seqgan-real.csv').iloc[:, : 3]
-    real_tg = pd.read_csv(directory + 'experiment-log-textgan-real.csv').iloc[:, : 3]
-    real_cg = pd.read_csv(directory + 'experiment-log-cgan-real.csv').iloc[:, : 3]
-    real_ig = pd.read_csv (directory + 'experiment-log-infogan-real.csv').iloc[:, : 3]
-    real_dg = pd.read_csv (directory + 'experiment-log-dcgan-real.csv').iloc[:, : 3]
-
-    # Create new dataframe to represent Oracle EmbeddingSimilarity across all models
-    oracle_embed = pd.DataFrame({'Epochs': [1, 6, 10],
-                                 'SeqGan': oracle_sg['EmbeddingSimilarity'],
-                                 'TextGan': oracle_tg['EmbeddingSimilarity'],
-                                 'CGan': oracle_cg['EmbeddingSimilarity'],
-                                 'InfoGan': oracle_ig['EmbeddingSimilarity'],
-                                 'DCGAN': oracle_dg['EmbeddingSimilarity'],
-                                 })
-
-    # Create new dataframe to represent Oracle nll-oracle across all models
-    oracle_nll_orc = pd.DataFrame({'Epochs': [1, 6, 10],
-                                   'SeqGan': oracle_sg['nll-oracle'],
-                                   'TextGan': oracle_tg['nll-oracle'],
-                                   'CGan': oracle_cg['nll-oracle'],
-                                   'InfoGan': oracle_ig['nll-oracle'],
-                                   'DCGAN': oracle_dg['nll-oracle'],
-                                   })
-
-    # Create new dataframe to represent Oracle nll-test across all models
-    oracle_nll_test = pd.DataFrame({'Epochs': [1, 6, 10],
-                                    'SeqGan': oracle_sg['nll-test'],
-                                    'TextGan': oracle_tg['nll-test'],
-                                    'CGan': oracle_cg['nll-test'],
-                                    'InfoGan': oracle_ig['nll-test'],
-                                    'DCGAN': oracle_dg['nll-test'],
-                                    })
-
-    # Create new dataframe to represent Real EmbeddingSimilarity across all models
-    real_embed = pd.DataFrame({'Epochs': [1, 6, 10],
-                               'SeqGan': real_sg['EmbeddingSimilarity'],
-                               'TextGan': real_tg['EmbeddingSimilarity'],
-                               'CGan': real_cg['EmbeddingSimilarity'],
-                               'InfoGan': real_ig['EmbeddingSimilarity'],
-                               'DCGAN': real_dg['EmbeddingSimilarity'],
-                               })
-
-    # Create new dataframe to represent Real nll-test across all models
-    real_nll_test = pd.DataFrame({'Epochs': [1, 6, 10],
-                                  'SeqGan': real_sg['nll-test'],
-                                  'TextGan': real_tg['nll-test'],
-                                  'CGan': real_cg['nll-test'],
-                                  'InfoGan': real_ig['nll-test'],
-                                  'DCGAN': real_dg['nll-test'],
-                                  })
+    # filter results to get separate lists for each metric type under each training
+    filter_col = [col for col in real_results if col.startswith('EmbSim_') ]
+    df_list.append(real_results[filter_col])
+    filter_col = [col for col in real_results if col.startswith('Nll-Test')]
+    df_list.append(real_results[filter_col])
+    filter_col = [col for col in oracle_results if col.startswith('EmbSim_')]
+    df_list.append(oracle_results[filter_col])
+    filter_col = [col for col in oracle_results if col.startswith('Nll-Test')]
+    df_list.append(oracle_results[filter_col])
+    filter_col = [col for col in oracle_results if col.startswith('Nll-Oracle')]
+    df_list.append(oracle_results[filter_col])
 
     # define number of rows and columns for subplots
     nrow = 3
-    ncol = 2
+    ncol = math.ceil(len(df_list) / nrow)
+
     # make a list of all dataframes
-    df_list = [oracle_embed, oracle_nll_orc, oracle_nll_test, real_embed, real_nll_test]
-    df_title_list = ['Oracle EmbeddingSimilarites', 'Oracle NLL-Oracle', 'Oracle NLL-Test',
-                     'Real EmbeddingSimilarites',
-                     'Real NLL-Test']
+    df_title_list = ['Real EmbeddingSimilarites', 'Real NLL-Test', 'Oracle EmbeddingSimilarites', 'Oracle NLL-Test', 'Oracle NLL-Oracle']
+    
     # plot counter
     count = 0
+    
+    #build plot
     fig, axes = plt.subplots(nrow, ncol)
     plt.subplots_adjust(wspace=0.2, hspace=0.5)
     for r in range(nrow):
         for c in range(ncol):
-            if count < 5:
-                df_list[count].plot(ax=axes[r, c], x='Epochs', y=['SeqGan', 'TextGan', 'CGan', 'InfoGan', 'DCGAN'], kind='line',
-                                    title=df_title_list[count], figsize=(20, 10))
+            if count < len(df_list):
+                df = df_list[count]
+                if df.columns.any('EmbSim_'):
+                    df.columns = df.columns.str.replace(r'^EmbSim_', '')
+                if df.columns.any('Nll-Test_'):
+                    df.columns = df.columns.str.replace(r'^Nll-Test_', '')
+                if df.columns.any('Nll-Oracle_'):
+                    df.columns = df.columns.str.replace(r'^Nll-Oracle_', '')
+                df.plot(ax=axes[r, c], y=df.columns, kind='line',
+                            title=df_title_list[count], figsize=(20, 10))
                 count += 1
     # save metrics to .png for later use in pdf report
     plt.savefig(directory + 'model_metric_charts.png')
